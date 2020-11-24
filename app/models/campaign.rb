40dceb21cd6ae57a5ad9569e599c267401c12d8e
ABR <h.abr3222@gmail.com>
2020-11-24 19:33:46 +0500

    add the searching scope to the filter the campaigns more quickely with the help of scope

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,11 @@
 class Campaign < ApplicationRecord
   belongs_to :user
+
+  scope :filter_by_title, -> (title) { where("title like ?", "#{title}%")}
+  scope :filter_by_user_id, -> (user_id) { where user_id: user_id }
+  scope :filter_by_purpose, -> (purpose) { where purpose: purpose }
+  scope :filter_by_estimated_duration, -> (estimated_duration) { where estimated_duration: estimated_duration }
+
+  # @campaign = Campaign.filter_by_user_id(2).filter_by_title("SDF")
+  #
 end
