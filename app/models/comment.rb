f497124aec329446c2bc47b40421a91324582c70
ABR <h.abr3222@gmail.com>
2021-01-22 12:51:27 +0500

    Set order testingn is over ride or not

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
-
   acts_as_sequenced scope: :campaign_id # Pick By Default sequential_id
   acts_as_sequenced column: :user_comment_number, scope: :user_id   #  Define the Column Name as well
 
+  before_validation :set_order_form_number
+
+  def set_order_form_number
+    # self.user_comment_number = 100
+  end
 end
